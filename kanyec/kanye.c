02b030a4e3abcb3a99becb858eabc7e81d0b53e4
Joel Richardson <39085573+JoelWantsCoffee@users.noreply.github.com>
2019-05-25 16:27:14 +1000

    Add files via upload

--- kanyec/kanye.c
+++ kanyec/kanye.c
@@ -7,22 +7,22 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h> 
 
 #include "../libs/vecmath.h"
 #include "../libs/surfaces.h"
 #include "../libs/cam3d.h"
 #include "../libs/shells.h"
+#include "../libs/utils.h"
 
-void loop(input *ins, int fc, void ** arg) {
+#include "open-simplex-noise.h"
+
+void loop(input *ins, int  fc, void ** arg) {
     camera c;
     initCamera(&c, 400, 100);
     mesh me;
 
     dupeMesh((mesh *) arg[1], &me);
-
-    //printf("%d", me.faceCount);
-
-    //inportObj((mesh*) arg[1], "cube.obj");
 
     vector mv = {
         .x = 0,
@@ -31,10 +31,16 @@ void loop(input *ins, int fc, void ** arg) {
         .w = 0,
     };
 
-    rotateMesh(&me, 3.1415, ((float) fc)/10, 0);
+    rotateMesh(&me, ((float) fc)/10, ((float) fc)/30, 0);
 
-    if (ins->on[4]) ((vector*) *arg)->y += 0.1;
-    if (ins->on[6]) ((vector*) *arg)->y -= 0.1;
+    if (ins->on[4]) ((vector*) *arg)->y -= 0.1;
+    if (ins->on[6]) ((vector*) *arg)->y += 0.1;
+
+    // if (((vector*) *arg)->y > 3.1) ((vector*) *arg)->y = -3.1;
+    // if (((vector*) *arg)->y < -3.1) ((vector*) *arg)->y = 3.1;
+
+    // if (((vector*) *arg)->w > 3.1) ((vector*) *arg)->w = -3.1;
+    // if (((vector*) *arg)->w < -3.1) ((vector*) *arg)->w = 3.1;
 
     if (ins->on[5]) ((vector*) *arg)->w += 0.1;
     if (ins->on[7]) ((vector*) *arg)->w -= 0.1;
@@ -59,13 +65,17 @@ void loop(input *ins, int fc, void ** arg) {
 
     c.pos.z = ((vector*) *arg)->z;
     c.pos.x = ((vector*) *arg)->x;
-    c.pos.y = ((vector*) *arg)->y;
+    c.vrot = ((vector*) *arg)->y;
     c.hrot = ((vector*) *arg)->w;
+
+    
 
     mesh3d(&c, me);
 
-    system("clear");
+    fputs("\033[1;1H", stdout);
     draw(c.image);
+    // glutDisplayFunc(drawGL);
+    // glutMainLoop();
 
     //draw(c.image);
 
@@ -79,21 +89,69 @@ void *gi(void *ins) {
     while (1) getIns((input *) ins);
 }
 
+void sigintHandler(int sig_num) { 
+    resetKeys();
+    exit(0);
+} 
+
 int main() {
+
     mesh me;
 
     vector pos = {
         .x = 0,
-        .y = 0,
-        .z = -5,
+        .y = 0.5,
+        .z = -10,
         .w = 0,
     };
 
-    void * arg [2];
+    void * arg [3];
 
-    inportObj(&me, "monkey.obj");
+    inportObj(&me, "tpot.obj");
+
+
+    struct osn_context * con;
+    open_simplex_noise(77374, &con);
 
     arg[0] = (void*) &pos;
+
+    // int d = 40;
+
+    // vector pts[d][d];
+
+    // initMesh(&me, d*d, d*d*2);
+
+    // me.faceCount = (d-1)*(d-1)*2;
+
+    // for (int i = 0; i < d; i++) {
+    //     for (int j = 0; j < d; j++) {
+    //         pts[i][j].x = i - d/2;
+    //         pts[i][j].z = j - d/2;
+    //         float y = 10;
+    //         int am = 20;
+    //         for (int k = 1; k<am; k++) {
+    //             y += open_simplex_noise3(con, ((float) i) * 0.3, ((float) j)*0.3, k*10) * map(k, 1, am, 1, 0);
+    //         }
+    //         pts[i][j].y = y;
+    //         pts[i][j].v = intC(cLerp(fColour(216, 255, 188), fColour(106, 124, 3), map(pts[i][j].y, 9, 11, 0, 1)));
+    //         me.pts[i + j*d] = pts[i][j];
+    //     }
+    // }
+
+    // for (int i = 0; i < d - 1; i++) {
+    //     for (int j = 0; j < d - 1; j++) {
+    //         int index = i + j*(d-1);
+            
+    //         me.faces[index * 2].p1 = &me.pts[i + j*d];
+    //         me.faces[index * 2].p2 = &me.pts[i + 1 + j*d];
+    //         me.faces[index * 2].p3 = &me.pts[i + (j+1)*d];
+
+    //         me.faces[index * 2 + 1].p2 = &me.pts[i + 1 + (j+1)*d];
+    //         me.faces[index * 2 + 1].p1 = &me.pts[i + 1 + j*d];
+    //         me.faces[index * 2 + 1].p3 = &me.pts[i + (j+1)*d];
+    //     }
+    // }
+
     arg[1] = (void*) &me;
 
     pthread_t getInputs;
@@ -111,10 +169,12 @@ int main() {
     atexit(resetKeys);
     //on_exit(resetKeys);
 
+    signal(SIGINT, sigintHandler); 
+
     while (1) {
         //getIns(&sh.ins);
         //pthread_create(&getInputs, NULL, &gi, &(sh.ins));
-        run(&sh, fc, arg, 30);
+        run(&sh, fc, arg, 15);
         fc++;
     }
     return 0;
